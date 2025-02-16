fb14a249aa6166a080508f3d2e743464fd18c687
Harshit Bhardwaj <harshitbhardwaj3333gmail.com>
2025-02-16 19:50:55 +0530

    Addes footer section

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -4,9 +4,9 @@ import { Links } from './Links';
 const Footer: React.FC = () => {
   return (
 
-    <footer className="bg-white rounded-lg shadow-sm dark:bg-neutral-800">
+    <footer className="w-full border-t border-neutral-700">
       <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
-        <div className="sm:flex sm:items-center sm:justify-between">
+        <div className="sm:flex sm:items-center sm:justify-between space-y-4">
           <Links/>
           <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
             <li>
@@ -23,8 +23,7 @@ const Footer: React.FC = () => {
             </li>
           </ul>
         </div>
-        <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-600 lg:my-8" />
-        <span className="block text-sm text-gray-400 sm:text-center dark:text-gray-400">© 2025 <a href="" className="hover:underline">Flowbite™</a>. All Rights Reserved.</span>
+        
       </div>
     </footer>
 
